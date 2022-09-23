2317faaa50268feea5195b8708246b9c348c2fc5
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-09-22 21:10:55 -0400

    removed default react, added apis, created search engine

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="text-center">
+          <header className="App-header">Dictionary</header>
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="App-footer">Coded by Renee Christian</footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
