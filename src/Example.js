5eaaa1fc22f33f0bf1c4b787a39aea75d1483471
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-09-26 18:57:40 -0400

    Edited the example component

--- src/Example.js
+++ src/Example.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 export default function Example(props){
-  console.log(props.example)
-    if (props.example){
+  console.log({props})
+  if (props.example){
     return (
       <div className="Example">
-        {props.example.map(function (example, index) {
-          return <span key={index}>{example}</span>;
-        })}
+        <span>{props.example}</span>;
       </div>
     );
   } else {
