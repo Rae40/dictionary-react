c56564f9edf124c1cf9a472e650c46308acd05d2
Rae40 <103543425+Rae40@users.noreply.github.com>
2022-10-01 20:37:43 -0400

    created Photos component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,15 +3,12 @@ import "./Phonetic.css";
 
 
 export default function Phonetic(props){
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio}>
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <div className="text">
-      {props.phonetic.text}
-      </div>
+      <div className="text">{props.phonetic.text}</div>
     </div>
   );
 }
